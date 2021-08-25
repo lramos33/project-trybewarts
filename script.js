b9cde21512b16232ae65305854f674b1c7f35313
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-08-25 15:55:43 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const enterButton = document.querySelector('#enter-button');
+
+function loginValidade() {
+  const inputEmail = document.querySelector('#email');
+  const inputPassword = document.querySelector('#password');
+
+  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+    alert('Olá, Tryber!')
+  } else {
+    alert('Email ou senha inválidos.')
+  }
+}
+
+enterButton.addEventListener("click", loginValidade)
